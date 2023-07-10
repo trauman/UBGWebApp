711dda3e4226c2bda40a62ea8ee36460242c67d5
Skribblez448 <lbunczak@gmail.com>
2023-07-10 00:10:04 -0500

    Added database integration
    Added TestModel, TestContext, and TestDatabaseCommands to create a new table an populate it with data. Added the connection string to appsettings.json.

--- UBGWebApp/Controllers/HomeController.cs
+++ UBGWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 using UBGWebApp.Models;
 
---
+++ UBGWebApp/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UBGWebApp.Models
+{
+    public class TestModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
